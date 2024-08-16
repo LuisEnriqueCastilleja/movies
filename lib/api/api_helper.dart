a453ea7551dee665b4ebda9623f02fd9ad4b88cd
LuisEnriqueCastilleja <luis_castilleja2800@outlook.com>
2024-08-16 15:07:31 -0600

    docs: Commented code

--- lib/api/api_helper.dart
+++ lib/api/api_helper.dart
@@ -10,18 +10,23 @@ class ApiBaseHelper {
   //Production o Staging
   final String _baseUrl = 'https://api.themoviedb.org/3/';
 
+//Metodo para mandar un GET a la API
   Future<dynamic> get(String url, [Map<String, String>? headers]) async {
+    //Completamos la URL para hacer la peticion a la API
     var uri = Uri.parse(_baseUrl + url);
     // ignore: prefer_typing_uninitialized_variables
     var responseJson;
+    //Para guardar el response de la API
 
     try {
+      //Hacemos la peticion GET a la API con los headers correspondientes
       final response = await http.get(uri, headers: {
         HttpHeaders.acceptHeader: 'application/json',
         HttpHeaders.contentTypeHeader: 'application/json',
         'Authorization':
             'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYjc4ZjMxODIyNWM4ZjQwZDY3NGUwNzExNzBmYThiZSIsIm5iZiI6MTcyMzU3NzQ3MS45MDYxOTMsInN1YiI6IjY2YmJiMjJjNThkNzcwNmVjNzJkNDA0YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AX3i8xKWWNx7chS2RrGwuvmQP1L8EhuS5zAxbfk4prU'
       });
+      //Lo mandamos al metodo _returnResponse
       responseJson = _returnResponse(response);
     } on SocketException {
       throw FetchDataException('No internet connection');
@@ -30,34 +35,43 @@ class ApiBaseHelper {
   }
 
   Future<dynamic> update(String url, dynamic body) async {
+    //Verificamos la conexion a internet
     var connectivityResult = await (Connectivity().checkConnectivity());
-
+    //Completamos la URL
     var uri = Uri.parse(_baseUrl + url);
+    //SI la conexion contiene none quiere decir que no tiene conexion a internet
     if (connectivityResult.contains(ConnectivityResult.none)) {
       try {
+        //Hacemos el PUT a la API para actualizar algun dato
         final response = await http
             .put(uri, body: body != null ? json.encode(body) : null, headers: {
           HttpHeaders.acceptHeader: 'application/json',
           HttpHeaders.contentTypeHeader: 'application/json'
         });
+        //Regresamos el response y aqui veremos si se realizo o no correctamete la edicion
         return response;
       } catch (error) {
         return error;
       }
     }
+    //Regresamos que no hay conexion en el dado caso que no tenga conexion a internet
     return ConnectivityResult.none;
   }
 
   Future<dynamic> delete(String url) async {
+    //Completamos la URL
     var uri = Uri.parse(_baseUrl + url);
 
     try {
+      //Hacemos el Delete a la API con sus headers correspondientes
       final response = await http.delete(uri, headers: {
         HttpHeaders.acceptHeader: 'application/json',
       });
+      //SI nos regresa un 200 quiere decir que se elimino correctamente el objeto o lo que queriamos que se eliminara
       if (response.statusCode == 200) {
         return true;
       }
+      //Si regresa un 400 o algun otro status code quiere decir que no se elimino y regresamos el status code
       if (response.statusCode == 400) {
         return response.statusCode;
       }
@@ -68,28 +82,37 @@ class ApiBaseHelper {
   }
 
   Future<dynamic> post(String url, dynamic body) async {
+    //Para verificar la conexion del dispositivo
     var connectivityResult = await (Connectivity().checkConnectivity());
 
+    //Completamos la URL
     var uri = Uri.parse(_baseUrl + url);
+    //SI la conexion contiene none quiere decir que no tiene conexion a internet
     if (connectivityResult.contains(ConnectivityResult.none)) {
       try {
+        //Hacemos el post a la API, mandamos el body que recibimos y lo codificamos
         final response =
             await http.post(uri, body: json.encode(body), headers: {
           HttpHeaders.acceptHeader: 'application/json',
           HttpHeaders.contentTypeHeader: 'application/json'
         });
+        //Regresamos la respuesta que nos manda la API
         return response;
       } catch (error) {
         return error;
       }
     }
+    //Regresamos que no hay conexion en el dado caso que no tenga conexion a internet
     return ConnectivityResult.none;
   }
 
   dynamic _returnResponse(http.Response response) {
+    //Verificamos el statusCode que tiene el response que nos devolvio la API
     switch (response.statusCode) {
       case 200:
+        //Si es 200 el status code lo decodificamos el json para traernos el body
         var responseJson = json.decode(response.body.toString());
+        //Devolvemos el body del Json
         return responseJson;
       case 400:
         throw BadRequestException(response.body.toString());
